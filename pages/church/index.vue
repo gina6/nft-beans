54698d5d48775990f8395411a36007aba3c45011
Niklas.Buchfink <niklas.buchfink@web.de>
2022-04-07 12:29:10 +0200

    add eatingbean gif

--- pages/church/index.vue
+++ pages/church/index.vue
@@ -12,7 +12,7 @@
       <span>Feed the beans.</span>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
     </div>
-    <img class="image" src="https://picsum.photos/503/669" alt="bean eating something" /> 
+    <img class="image" src="/eatingbean.gif" alt="bean eating something" /> 
   </div>
   <div class="divider"></div>
   <div class="feed-container">

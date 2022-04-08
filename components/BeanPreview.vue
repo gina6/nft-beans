ea4169f756ee6b8958c5119d0075db21bdbb268a
gina6 <gina.fontana@stud.hslu.ch>
2022-04-08 09:37:28 +0200

    add placeholder for evolved bean

--- components/BeanPreview.vue
+++ components/BeanPreview.vue
@@ -1,38 +1,46 @@
 <script setup>
-  const bean = defineProps(['text', 'smallText', 'image'])
+const bean = defineProps(["text", "smallText", "image"]);
+
+const imgError = ref(false);
 </script>
 
 <template>
   <div class="bean-container">
-    <img :src="bean.image" :alt="bean.text">
+    <img
+      v-if="!imgError"
+      :src="bean.image"
+      :alt="bean.text"
+      @error="imgError = true"
+    />
+    <BeanPlaceholder v-else> evolved bean </BeanPlaceholder>
     <div class="bean-text">
-      {{bean.text}}
+      {{ bean.text }}
       <p v-if="bean.smallText" class="small">{{ bean.smallText }}</p>
     </div>
   </div>
 </template>
 
 <style lang="scss" scoped>
-  .bean-container {
-    height: fit-content;
-    width: min-content;
+.bean-container {
+  height: fit-content;
+  width: min-content;
 
-    img {
-      width: 250px;
-      height: 250px;
-      object-fit: contain;
-      border-radius: 10px;
-    }
-    .bean-text {
-      @include helvetica;
-      font-size: 20px;
-      text-align: center;
+  img {
+    width: 250px;
+    height: 250px;
+    object-fit: contain;
+    border-radius: 10px;
+  }
+  .bean-text {
+    @include helvetica;
+    font-size: 20px;
+    text-align: center;
 
-      > .small {
-        font-size: 12px;
-        opacity: 0.8;
-        margin-top: 4px;
-      }
+    > .small {
+      font-size: 12px;
+      opacity: 0.8;
+      margin-top: 4px;
     }
   }
+}
 </style>
